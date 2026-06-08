1fb5ab3834bb10710431b09f17f6716ea14245f5
Tieen001 <134282928+Tieen001@users.noreply.github.com>
2026-06-09 01:05:01 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const contactRouter = require("./app/routes/contact.route");
+app.use("/api/contacts", contactRouter);
+
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to contact book application." });
 });
